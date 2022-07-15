01789cd0c63f74ebac48ac071678dbcd8b30ad96
RostMush <95547671+rostyslavMushynskyi@users.noreply.github.com>
2022-07-15 23:09:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 "use strict";
-// const user = {};
+const user = {};
 
-// user.userName = "Rostyslav";
+user.userName = "Rostyslav";
 
-// user.age = 15;
+user.age = 15;
 
-// user.showUserNam = function () {
-//   console.log(user.userName);
-// };
+user.showUserNam = function () {
+  console.log(user.userName);
+};
 
-// user.showUserNam();
+user.showUserNam();
 
-// user.updateAge = function () {
-//   user.age = user.age + 1;
-// };
+user.updateAge = function () {
+  user.age = user.age + 1;
+};
 
-// user.updateAge();
+user.updateAge();
 
-// console.log(user);
+console.log(user);
 
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
 
-// const salariesGlobal = Object.values(salaries);
+const salariesGlobal = Object.values(salaries);
 
-// let sum = 0;
+let sum = 0;
 
-// for (const salary of salariesGlobal) {
-//   sum += salary;
-// }
+for (const salary of salariesGlobal) {
+  sum += salary;
+}
 
-// console.log(sum);
+console.log(sum);
 
-// const calculator = {
-//   read(a, b) {
-//     calculator.a = a;
-//     calculator.b = b;
-//   },
-//   sum() {
-//     return calculator.a + calculator.b;
-//   },
-//   mult() {
-//     return calculator.a * calculator.b;
-//   },
-// };
+const calculator = {
+  read(a, b) {
+    calculator.a = a;
+    calculator.b = b;
+  },
+  sum() {
+    return calculator.a + calculator.b;
+  },
+  mult() {
+    return calculator.a * calculator.b;
+  },
+};
 
-// calculator.read(5, 10);
+calculator.read(5, 10);
 
-// console.log(calculator.sum());
+console.log(calculator.sum());
 
-// console.log(calculator.mult());
+console.log(calculator.mult());
 
-// const cafe = {
-//   name: "Republic",
-//   width: 200,
-//   height: 300,
-//   personal: 5,
-//   hr: true,
-// };
+const cafe = {
+  name: "Republic",
+  width: 200,
+  height: 300,
+  personal: 5,
+  hr: true,
+};
 
-// Object.entries(cafe);
+Object.entries(cafe);
 
-// const area = cafe.width * cafe.height;
-// console.log(area);
+const area = cafe.width * cafe.height;
+console.log(area);
