671450cadc539794a79be7a21e108467876c2b3f
RostMush <95547671+rostyslavMushynskyi@users.noreply.github.com>
2022-07-15 23:07:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+"use strict";
+// const user = {};
+
+// user.userName = "Rostyslav";
+
+// user.age = 15;
+
+// user.showUserNam = function () {
+//   console.log(user.userName);
+// };
+
+// user.showUserNam();
+
+// user.updateAge = function () {
+//   user.age = user.age + 1;
+// };
+
+// user.updateAge();
+
+// console.log(user);
+
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// const salariesGlobal = Object.values(salaries);
+
+// let sum = 0;
+
+// for (const salary of salariesGlobal) {
+//   sum += salary;
+// }
+
+// console.log(sum);
+
+// const calculator = {
+//   read(a, b) {
+//     calculator.a = a;
+//     calculator.b = b;
+//   },
+//   sum() {
+//     return calculator.a + calculator.b;
+//   },
+//   mult() {
+//     return calculator.a * calculator.b;
+//   },
+// };
+
+// calculator.read(5, 10);
+
+// console.log(calculator.sum());
+
+// console.log(calculator.mult());
+
+// const cafe = {
+//   name: "Republic",
+//   width: 200,
+//   height: 300,
+//   personal: 5,
+//   hr: true,
+// };
+
+// Object.entries(cafe);
+
+// const area = cafe.width * cafe.height;
+// console.log(area);
